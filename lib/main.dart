eb759ffa2f5080c97e85f1fbe5e2d488529c36a6
中野勇貴 <sakeikura325325@gmail.com>
2022-04-16 15:37:22 +0900

    Add: Navigatorの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screen/product_detail_screen.dart';
 import 'package:shop_app/screen/products_overview_screen.dart';
 
 void main() {
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
         fontFamily: "Lato",
       ),
       home: ProductsOverViewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (context) => const ProductDetailScreen(),
+      },
     );
   }
 }
--- lib/screen/product_detail_screen.dart
+++ lib/screen/product_detail_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
+  static const routeName = "/product-detail";
+
   const ProductDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("title"),
+      ),
     );
   }
 }
