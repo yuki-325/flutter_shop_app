7d70af387a1b69a7302ae9dc425042687e81b514
中野勇貴 <sakeikura325325@gmail.com>
2022-04-17 17:11:40 +0900

    Add: Providerから値を取得する処理の追加

--- lib/screen/product_detail_screen.dart
+++ lib/screen/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = "/product-detail";
@@ -8,9 +10,21 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+
+    /**
+     * NOTE 引数listenの説明
+     * true : 変更が notify されるとリビルドが起こる
+     * false: 値の変更に応じた処理（表示更新など）が起こらなくなる
+     */
+    final loadProducts = Provider.of<Products>(
+      context,
+      listen: false,
+    );
+    final loadProduct = loadProducts.findById(id: productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("title"),
+        title: Text(loadProduct.title),
       ),
     );
   }
