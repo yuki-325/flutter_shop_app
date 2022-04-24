426404e8e867a9b309ab8e9f3c88b3605e1ebba1
中野勇貴 <sakeikura325325@gmail.com>
2022-04-24 16:41:51 +0900

    Add: 注文一覧画面に注文詳細を表示する機能を追加

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -35,7 +35,7 @@ class CartScreen extends StatelessWidget {
                   ),
                   Chip(
                     label: Text(
-                      "\$${cart.totalAmount}",
+                      "\$${cart.totalAmount.toStringAsFixed(2)}",
                       style: const TextStyle(color: Colors.white),
                     ),
                     backgroundColor: Theme.of(context).colorScheme.primary,
--- lib/widgets/order_item_widget.dart
+++ lib/widgets/order_item_widget.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:intl/intl.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem order;
   const OrderItemWidget({
     Key? key,
@@ -10,21 +12,64 @@ class OrderItemWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<OrderItemWidget> createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
       child: Column(
         children: [
           ListTile(
-            title: Text("\$${order.amount}"),
+            title: Text("\$${widget.order.amount}"),
             subtitle: Text(
-              DateFormat("yyyy/MM/dd hh:mm").format(order.dateTime),
+              DateFormat("yyyy/MM/dd hh:mm").format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: const Icon(Icons.expand),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(widget.order.products.length * 20.0 + 100.0, 100),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "\$${prod.quantity}x \$${prod.price}",
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
