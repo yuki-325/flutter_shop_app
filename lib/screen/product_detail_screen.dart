382094ed9a1306500ccff6ba70b27cb6d12f0dd6
中野勇貴 <sakeikura325325@gmail.com>
2022-04-24 14:22:04 +0900

    Fix: product_detail_screen 商品の詳細を表示

--- lib/screen/product_detail_screen.dart
+++ lib/screen/product_detail_screen.dart
@@ -26,6 +26,38 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "\$${loadProduct.price}",
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              loadProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
